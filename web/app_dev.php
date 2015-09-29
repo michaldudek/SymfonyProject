2dedc2ffecec45f3acaeb76e512483225f749237
Michał Pałys-Dudek <michal@michaldudek.pl>
2015-09-30 00:27:19 +0200

    Added easy and quick access blocker based on IP's.

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,25 +2,15 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
+use MD\Flavour\Bootstrap\Blocker;
 
 use MyApp\MyApp;
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(
-        in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-        || php_sapi_name() === 'cli-server'
-        // allow in .dev server (vagrant)
-        || preg_match('/\.dev$/', @$_SERVER['HTTP_HOST'])
-    )
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+require_once __DIR__.'/../.cache/bootstrap.php.cache';
 
-$loader = require_once __DIR__.'/../.cache/bootstrap.php.cache';
+// check access to this script
+Blocker::check($_SERVER);
+
 Debug::enable();
 
 $kernel = new MyApp('dev', true);
